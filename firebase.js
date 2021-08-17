944c53d2ea5715e14bdcf878f9978054a48740af
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-08-17 15:39:32 +0530

    Cart page done.

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBY4sUaGnOTCLU_xV4J-8kPXtPyN_cagK0",
+    authDomain: "clone-9d14f.firebaseapp.com",
+    projectId: "clone-9d14f",
+    storageBucket: "clone-9d14f.appspot.com",
+    messagingSenderId: "12744503058",
+    appId: "1:12744503058:web:a3562351677993d3dd8dcb",
+    measurementId: "G-593D3RZ2SD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
