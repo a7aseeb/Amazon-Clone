ffcd7cf8efd8c7c75aeba9bca1e21eabc3ce488b
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-08-18 12:46:13 +0530

    working with firebase.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,16 @@
-var firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
     apiKey: "AIzaSyBY4sUaGnOTCLU_xV4J-8kPXtPyN_cagK0",
     authDomain: "clone-9d14f.firebaseapp.com",
     projectId: "clone-9d14f",
     storageBucket: "clone-9d14f.appspot.com",
     messagingSenderId: "12744503058",
-    appId: "1:12744503058:web:a3562351677993d3dd8dcb",
-    measurementId: "G-593D3RZ2SD"
+    appId: "1:12744503058:web:0a8e6142a8f6e461dd8dcb",
+    measurementId: "G-VCKX38G5LZ"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+
+  var db = firebase.firestore();
---
+++ script.js
@@ -0,0 +1,20 @@
+
+function getItems(){
+    db.collection("items").get().then((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                image: doc.data().image,
+                name: doc.data().name,
+                make: doc.data().make,
+                rating: doc.data().rating,
+                price: doc.data().price
+
+            })
+        });
+        
+    });    
+}
+
+getItems();
